9445915fb8913efe13faa30bbd8d5f863851f749
ccdera-hub <ccdera@gmail.com>
2025-08-25 22:48:09 +0100

    Update ChangeMachine.py

--- ChangeMachine.py
+++ ChangeMachine.py
@@ -1,7 +1,6 @@
 #Change machine: Ask for an amount of pennies as an integer. Print how many pounds, pence, and leftover pennies that makes using UK currency breakdown:
 #1 pound = 100 pence.
 #Also break the remaining pence into 50p, 20p, 10p, 5p, 2p, 1p coins (use // and %).
-from itertools import count
 
 pennies = int(input("Pennies: "))
 pounds = pennies // 100 # how many pounds
@@ -11,4 +10,5 @@ print(f"{pounds} pound(s), {remaining} pence(s)")
 for coin in [50, 20, 10, 5, 2, 1]:
     count = remaining // coin
     remaining %= coin
+
     print(f"{coin}p x {count}")
